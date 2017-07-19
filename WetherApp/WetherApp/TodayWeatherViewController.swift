9a450733b4df5c02b4ed5eeb2d48436687f4c301
Konstantin Khokhlov <sqb@mail.ru>
2017-07-19 17:32:43 +0400

    STORYBOARDS: Main, TodayWeather, Forecast - ADDED

---
+++ WetherApp/WetherApp/TodayWeatherViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TodayWeatherViewController.swift
+//  WetherApp
+//
+//  Created by Konstantin Khokhlov on 19.07.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import UIKit
+
+class TodayWeatherViewController: UIViewController {
+}
