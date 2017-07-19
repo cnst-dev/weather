e8d325d7d3c674756c55e8c4a76483aa8df40046
Konstantin Khokhlov <sqb@mail.ru>
2017-07-19 15:51:30 +0400

    Start

---
+++ WetherApp/WetherApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  WetherApp
+//
+//  Created by Konstantin Khokhlov on 19.07.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
